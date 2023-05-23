d416bb2c34f802277b8045ee858ee49984562f25
Ekaterina Geleckaya <geleczkak26@gmail.com>
2023-05-23 17:28:31 +0300

    Add input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from "react";
+
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,9 +28,10 @@ const App = () => {
 
   return (
     <div>
-      <Expenses items={expenses}/>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
