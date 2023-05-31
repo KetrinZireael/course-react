c93d47e2d1c9fec1552d7a3222a245b5da8656d0
Ekaterina Geleckaya <geleczkak26@gmail.com>
2023-05-31 18:04:14 +0300

    Add Chart and ChartBar

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+import ChartBar from "./ChartBar";
+import './Chart.css'
+
+const Chart = (props) => {
+    return (
+        <div className="chart">
+            
+        </div>
+    );
+};
+
+export default Chart;
